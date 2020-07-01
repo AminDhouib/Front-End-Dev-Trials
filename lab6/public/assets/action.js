8cda2e048ae4f73448de1dcfc7a269a35046af6e
AminDhouib <adhou064@uottawa.ca>
2020-06-30 21:15:14 -0400

    Commit lab6

---
+++ lab6/public/assets/action.js
@@ -0,0 +1,24 @@
+// jQuery that will "listen" to the html niceSurvey.html
+$(document).ready(function () {
+
+  $('form').on('submit', function () {
+
+    // var item = $('form input');
+    // console.log(item.serializeArray());
+    console.log("My data", $(this).serializeArray());
+
+    $.ajax({
+      type: 'POST',
+      url: '/niceSurvey',
+      data: $(this).serializeArray(),
+      success: function (data) {
+        // do something with the data via front-end framework
+        // Make the submit button red, disabled and saying Thank you
+        $("#bb").css("background-color", "red");
+        $("#bb").prop("disabled", "true");
+        $("#bb").text("Thank you!");
+      }
+    });
+    return false;
+  });
+});
